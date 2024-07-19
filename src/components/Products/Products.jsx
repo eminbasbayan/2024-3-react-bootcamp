84499ba9a7b67b9fd5cdb9b645242a65e6a771b2
eminbasbayan <emin.basbayan@icloud.com>
2024-07-19 23:37:34 +0300

    fetch products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import ProductItem from "./ProductItem";
 import AddNewProduct from "./AddNewProduct";
-import { productsData } from "../../data";
 import "./Products.css";
 
 function Products() {
-  const [products, setProducts] = useState(productsData);
+  const [products, setProducts] = useState([]);
 
   function handleSubmit(productData) {
     const newProduct = {
@@ -17,9 +16,16 @@ function Products() {
     setProducts([newProduct, ...products]);
   }
 
+  function fetchProducts() {
+    fetch("https://fakestoreapi.com/products/")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }
+
   return (
     <div className="products">
       <h2 className="text-4xl font-bold mb-5">Products Component</h2>
+      <button onClick={fetchProducts} className="btn btn-primary mb-10">Get All Products</button>
       <AddNewProduct handleSubmit={handleSubmit} />
       <div className="products-wrapper">
         {products.map((product) => (
