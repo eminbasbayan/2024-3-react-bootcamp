8a69eb1cd2dde2c4a8a10c6b0d13e8e4eddc16e9
eminbasbayan <emin.basbayan@icloud.com>
2024-07-19 23:09:16 +0300

    click out side kullanımı

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -7,8 +7,8 @@ function Modal({ setIsShowModal, title, desc }) {
   }
 
   return createPortal(
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white rounded-lg shadow-lg p-6 max-w-lg w-full relative">
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="bg-white rounded-lg shadow-lg p-6 max-w-lg w-full relative z-50">
         <div className="flex justify-between items-center border-b pb-3">
           <h3 className="text-xl font-semibold">{title}</h3>
           <button
@@ -34,6 +34,10 @@ function Modal({ setIsShowModal, title, desc }) {
           </button>
         </div>
       </div>
+      <div
+        className="bg-black bg-opacity-50 absolute w-full h-full"
+        onClick={handleClose}
+      ></div>
     </div>,
     document.getElementById("modal")
   );
