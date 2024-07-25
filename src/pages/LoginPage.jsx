504c0a8f18211c94d7c0b433d0bc5358f806a7aa
eminbasbayan <emin.basbayan@icloud.com>
2024-07-25 23:39:14 +0300

    formik kullanımı

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,22 @@
+import { useFormik } from "formik";
+
 const LoginPage = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      remember_me: false,
+    },
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm();
+    },
+  });
   return (
     <div className="bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="w-full max-w-md bg-white shadow-md rounded-lg p-8">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={formik.handleSubmit}>
           <div>
             <label
               htmlFor="email"
@@ -15,7 +28,9 @@ const LoginPage = () => {
               type="email"
               id="email"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              required
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
             />
           </div>
           <div>
@@ -29,7 +44,9 @@ const LoginPage = () => {
               type="password"
               id="password"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              required
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
             />
           </div>
           <div className="flex items-center justify-between">
@@ -39,6 +56,7 @@ const LoginPage = () => {
                 name="remember_me"
                 type="checkbox"
                 className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                onChange={formik.handleChange}
               />
               <label
                 htmlFor="remember_me"
