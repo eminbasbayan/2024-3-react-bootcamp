1cff5e2028ff3c5eb51cacbd2c44f30f5230b7d0
eminbasbayan <emin.basbayan@icloud.com>
2024-07-26 21:23:36 +0300

    register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 // import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
+// import LoginPage from "./pages/LoginPage";
+
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <div className="app p-10">
       {/* <HomePage /> */}
-      <LoginPage />
+      {/* <LoginPage /> */}
+      <RegisterPage />
     </div>
   );
 }
