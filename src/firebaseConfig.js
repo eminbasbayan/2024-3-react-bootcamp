2fc35ab9cfb25f82473abd483322103e19b2345f
Emin Basbayan <emin.basbayan@icloud.com>
2024-08-14 21:42:54 +0300

    firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCDW8bYXT4HdCKqVcoP2PJzzIaBS6DH0E",
+  authDomain: "e-commerce-c04ab.firebaseapp.com",
+  projectId: "e-commerce-c04ab",
+  storageBucket: "e-commerce-c04ab.appspot.com",
+  messagingSenderId: "112863834336",
+  appId: "1:112863834336:web:f9043920d7beec76686085",
+  measurementId: "G-17CP7E4TV9"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
