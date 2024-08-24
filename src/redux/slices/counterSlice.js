5d3be1e52cb2f163cacfa617d2ca7a57d4fa9496
Emin Basbayan <emin.basbayan@icloud.com>
2024-08-24 09:37:47 +0300

    redux toolkit kurulumu ve counterSlice

---
+++ src/redux/slices/counterSlice.js
@@ -0,0 +1,21 @@
+// count state, arttir, azalt
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    count: 0,
+  },
+  reducers: {
+    arttir: (state) => {
+      state.count += 1;
+    },
+    azalt: (state) => {
+      state.count -= 1;
+    },
+  },
+});
+
+export const { arttir, azalt } = counterSlice.actions;
+
+export default counterSlice;
